d8501ee8b263fcc97d04f14c3b6a409f788a6cb2
unknown <>
2016-09-24 11:41:42 +0800

    增加了try...except语句，提高容错性

--- tianqi.py
+++ tianqi.py
@@ -1,16 +1,6 @@
 #coding=utf-8
 import requests
 
-
-cityname = raw_input('你想查看那个城市的天气?\n')
-cityname = cityname.decode('gbk').encode('cp936') 
-url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityname
-
-r = requests.get(url)
-data = r.json()
-
-result = data['data']
-
 def weathe_detail(time,title,a=False):
     if a is not False:
         dic = result[time][a]
@@ -27,18 +17,30 @@ def weathe_detail(time,title,a=False):
         b+=1
     print "\n"
 
-forecast_title = [u'风向',u'最高温度',u'风力',u'日期',u'类型',u'最低温度']
-yesterday_title = [u'风向',u'类型',u'最高温度',u'最低温度',u'日期',u'风力']
+cityname = raw_input('你想查看那个城市的天气?\n')
+cityname = cityname.decode('gbk').encode('utf-8') 
+url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityname
+try:
+	r = requests.get(url)
+	data = r.json()
+	result = data['data']
+	forecast_title = [u'风向',u'最高温度',u'风力',u'日期',u'类型',u'最低温度']
+	yesterday_title = [u'风向',u'类型',u'最高温度',u'最低温度',u'日期',u'风力']
 
-print u'城市:',result['city'];
-weathe_detail(a=0,time='forecast',title=forecast_title);
-weathe_detail(time='yesterday',title=yesterday_title);
-print u'感冒指数:',result['ganmao'];
-print u'温度:',result['wendu'];
-print u'aqi:',result['aqi'];
-b = int(raw_input(u"如果要查询未来5天天气，请输入需要查询的未来天数(例如:查询明天请按1)，按其他键退出:"))
-if b <= 4 and b >= 0:
-    weathe_detail(a=b-1,time='forecast',title=forecast_title);
+	try:
+		print u'城市:',result['city'];
+		weathe_detail(a=0,time='forecast',title=forecast_title);
+		weathe_detail(time='yesterday',title=yesterday_title);
+		print u'感冒指数:',result['ganmao'];
+		print u'温度:',result['wendu'];
+		print u'aqi:',result['aqi'];
+		b = int(raw_input(u"输入要查询的未来天数(1-5):"))
+		if b <= 4 and b >= 0:
+		    weathe_detail(a=b-1,time='forecast',title=forecast_title);
 
-else:
-    exit()
+		else:
+		    exit()
+	except:
+		print u'查询失败！'
+except:
+	print u"连接超时或者输入城市有误！"
